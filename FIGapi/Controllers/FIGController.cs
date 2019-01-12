221811da9fb58439047a71e9080015ece9062c16
Blake Robert <Blaker.blaker4@gmail.com>
2019-01-12 00:53:37 -0500

    App Cleanup

--- FIGapi/Controllers/FIGController.cs
+++ FIGapi/Controllers/FIGController.cs
@@ -4,11 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FIGapi.Models;
-using System.Web;
-using System.Data.Entity;
 using System;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace FIGapi.Controllers
 {
--- FIGapi/Startup.cs
+++ FIGapi/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
-using System.Web.Http;
 using System.Buffers;
 
 namespace FIGapi
